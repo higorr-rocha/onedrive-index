9c4a00403196dc85dc9746f7be31107c2c690086
ShadowG25OQ <72710977+ShadowG25OQ@users.noreply.github.com>
2024-02-19 01:25:16 -0300

    Updating Icons Pt. 3

--- src/components/SwitchLayout.tsx
+++ src/components/SwitchLayout.tsx
@@ -1,6 +1,5 @@
 import { Fragment } from 'react'
 import { IconProp } from '@fortawesome/fontawesome-svg-core'
-import { faList, faGrip } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Listbox, Transition } from '@headlessui/react'
 import { useTranslation } from 'next-i18next'
@@ -8,8 +7,8 @@ import { useTranslation } from 'next-i18next'
 import useLocalStorage from '../utils/useLocalStorage'
 
 export const layouts: Array<{ id: number; name: 'Grid' | 'List'; icon: IconProp }> = [
-  { id: 1, name: 'List', icon: faList },
-  { id: 2, name: 'Grid', icon: faGrip },
+  { id: 1, name: 'List', icon: 'th-list' },
+  { id: 2, name: 'Grid', icon: 'th' },
 ]
 
 const SwitchLayout = () => {
